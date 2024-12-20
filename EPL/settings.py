0925b02f524053ae6490d2249c7325df37c96380
Naf_Second <98323709+Naf-Second@users.noreply.github.com>
2024-12-20 14:47:06 +0600

    Update settings.py

--- EPL/settings.py
+++ EPL/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'EPL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'EPL',
-#         'USER': 'postgres',
-#         'PASSWORD': '12345',
-#         'HOST': 'localhost',  # or the IP address of your PostgreSQL server
-#         'PORT': '5432',       # default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Supabase database name
+        'USER': 'postgres.quubglmmaacppqvixihf',  # Supabase user
+        'PASSWORD': '3*QaGa*WpECQ-xJ',   # Supabase password
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',           # Supabase host (e.g., db.xxxxxx.supabase.co)
+        'PORT': '6543',                # Supabase port (default is 5432)
+    }
+}
 
 # DATABASES = {
 #     'default': {
